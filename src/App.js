7f50e8c22c860d9de61fa7e1279900488194ece5
Ayesh Rodrigo <ayeshrodz@gmail.com>
2024-09-12 22:15:47 +1200

    Working skelton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import * as XLSX from "xlsx";
+import { saveAs } from "file-saver";
 
 function App() {
+  const [excelFile, setExcelFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    setExcelFile(e.target.files[0]);
+  };
+
+  const convertToJson = () => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const data = new Uint8Array(event.target.result);
+      const workbook = XLSX.read(data, { type: "array" });
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+      const json = XLSX.utils.sheet_to_json(worksheet);
+      const jsonBlob = new Blob([JSON.stringify(json, null, 2)], {
+        type: "application/json",
+      });
+      saveAs(jsonBlob, "converted.json");
+    };
+    reader.readAsArrayBuffer(excelFile);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Excel to JSON Converter</h1>
+      <input type="file" accept=".xlsx, .xls" onChange={handleFileChange} />
+      <button onClick={convertToJson} disabled={!excelFile}>
+        Convert and Download JSON
+      </button>
     </div>
   );
 }
